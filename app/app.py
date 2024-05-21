deeb5ed08436f5171b2d75effc9c55d2d86b7b47
Yogesh <yogesh@rsocial.in>
2024-05-21 15:59:21 +0800

    add docker-compose file changes

---
+++ app/app.py
@@ -0,0 +1,41 @@
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from fastapi import FastAPI
+import os
+
+resource = Resource(attributes={
+    "service.name": "FastAPI"
+})
+
+# Configure OTLP exporter to send traces to your collector endpoint
+otlp_exporter = OTLPSpanExporter(
+    endpoint= 'http://otel-collector:4317',
+    # endpoint=os.environ.get("OTLP_ENDPOINT", "http://otel-collector:4318"),
+    insecure=True
+)
+
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer = trace.get_tracer(__name__)
+
+# Use the OTLP exporter with BatchSpanProcessor
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(otlp_exporter)
+)
+
+app = FastAPI()
+
+# Instrument your FastAPI application
+FastAPIInstrumentor().instrument_app(app)
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
